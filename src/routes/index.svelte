1923a7325a2b765ecec22fa8c38aeea9fd6bb47a
Nils <nils.goksor@gmail.com>
2021-11-14 15:47:56 +0100

    feat: init commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,32 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+	import data from '../beer-data.json';
+</script>
+
+<script lang="ts">
+	let searchTerm = '';
+	let searchResults = [];
+</script>
+
+<svelte:head>
+	<title>Ölklubben utankrus</title>
+</svelte:head>
+
+<section>
+	<h1>Sök på en öl, land eller bryggeri</h1>
+	<input bind:value={searchTerm} placeholder="Sök på en öl" />
+	<div>
+		<ul>
+			{#each searchResults as { name }, i}
+				<li>
+					<p>
+						{i + 1}: {name}
+					</p>
+				</li>
+			{/each}
+		</ul>
+	</div>
+</section>
+
+<style>
+</style>
---
+++ src/routes/statistics.svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Ölklubben utankrus</title>
+</svelte:head>
+
+<section />
+
+<style>
+</style>
